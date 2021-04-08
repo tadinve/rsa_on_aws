f95a4fb664fc519e8a418ba37cf59a9d7973cc4c
vijayrgopu <vijay.gopu@gmail.com>
2021-04-08 15:04:53 -0700

    removed $ sign

--- src/infrastucture_install_files/terraform_files/redshift_cluster.tf
+++ src/infrastucture_install_files/terraform_files/redshift_cluster.tf
@@ -1,8 +1,8 @@
 resource "aws_redshift_cluster" "redshift-cadabra" {
   cluster_identifier  = "tf-redshift-cluster"
   database_name       = "dev"
-  master_username     = ${var.redshift_user}
-  master_password     = ${var.redshift_password}
+  master_username     = var.redshift_user
+  master_password     = var.redshift_password
   node_type           = "dc2.large"
   cluster_type        = "single-node"
   skip_final_snapshot = true
