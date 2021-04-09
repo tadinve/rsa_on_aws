d03354f0e051457b696a7bc3e08704f284cc7cd8
vijayrgopu <vijay.gopu@gmail.com>
2021-04-08 19:11:12 -0700

    seperated s3 buckets

--- src/infrastucture_install_files/terraform_files/s3_failed_weblogs_bucket.tf
+++ src/infrastucture_install_files/terraform_files/s3_failed_weblogs_bucket.tf
@@ -1,10 +1,3 @@
-resource "aws_s3_bucket" "target-s3bucket" {
-  bucket = "tf-${var.name_initials}-cadabra"
-  acl    = "private"
-
-  tags = local.common_tags
-}
-
 resource "aws_s3_bucket" "failed-logs-s3bucket" {
   bucket = "tf-${var.name_initials}-failed-http-logs"
   acl    = "private"
